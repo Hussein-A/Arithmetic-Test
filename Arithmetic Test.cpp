b39e492d3247c8de7695e4263fc58a74503de7a3
Hussein-A <43759836+Hussein-A@users.noreply.github.com>
2018-12-01 16:58:07 -0500

    Settings saved, bug fixes and new features
    --In addition to timestamping results all settings are saved to a file called "results.txt". This includes the operations used previously, their associated ranges and the time limit.
    
    --Settings saved can be used for prompt use without having to input settings manually each time. Only the most recent settings are used.
    
    --Answers and operations can now be negative ex. 75+-99 =  ? Chance for a negative operand is (1/5).
    
    --Fixed a bug where operands were not following their user inputted ranges.

--- Arithmetic Test.cpp
+++ Arithmetic Test.cpp
@@ -10,22 +10,79 @@
 
 using namespace std;
 
-struct result {
-
-	int score{0};
-	time_t ltime {time(NULL)};
+//Error handling. Note the following was borrowed from Strouroup's Programming: Principles and Practice using C++
+struct Exit : runtime_error {
+	Exit() : runtime_error("Exit") {}
 };
 
-result lresult; //will be modified by each "operation"_op depending on right or wrong answer
+// error() simply disguises throws:
+inline void error(const string& s)
+{
+	throw runtime_error(s);
+}
+
+inline void error(const string& s, const string& s2)
+{
+	error(s + s2);
+}
+
+inline void error(const string& s, int i)
+{
+	ostringstream os;
+	os << s << ": " << i;
+	error(os.str());
+}
+//End of Error Handling
+
 
 enum class operation {
 	plus = 1, minus, product, division
 };
 
-operation int_to_op(int x)
+operation int_to_op(int x)//simple error check for integer to associated operation
 {
-	if (x<int(operation::plus) || int(operation::division) < x) cerr <<"bad operation \n";
+	if (x<int(operation::plus) || int(operation::division) < x) error ("No such operation for given int. \n");
 	return operation(x);
+}
+
+string op_to_string(const operation& op) {//used when writing to file what op was used
+	switch (op) {
+	case operation::plus:
+		return "plus";
+		break;
+	case operation::minus:
+		return "minus";
+		break;
+	case operation::product:
+		return "product";
+		break;
+	case operation::division:
+		return "division";
+		break;
+	default:
+		return "error no such operation";
+		break;
+	}
+
+	return "Error unkown operation received. \n";
+}
+
+operation string_to_op(const string& s) {//used when writing to file what op was used
+	if (s == "plus") {
+		return operation::plus;
+	}
+	else if (s == "minus") {
+		return operation::minus;
+	}
+	else if (s == "product") {
+		return operation::product;
+	}
+	else if (s == "division") {
+		return operation::division;
+	}
+	else {
+		error("error no such operation");
+	}
 }
 
 
@@ -50,15 +107,12 @@ private:
 class op_range {//stores operation and associated range for left and right num
 
 public:
-	//constructors
 	op_range() {}
 
 	op_range(operation x, range l, range r ) {
-
 		operation = x;
 		leftrange = l;
 		rightrange = r;
-	
 	}
 
 	operation op() const { return operation; }
@@ -71,51 +125,133 @@ private:
 	range rightrange{range()};//default '+', on range (int) [0,1] for both ranges
 };
 
-//Error handling. Note the following was borrowed from Strouroup's Programming: Principles and Practice using C++
-struct Exit : runtime_error {
-	Exit() : runtime_error("Exit") {}
+
+struct result {//record, keeping the score and current time
+
+	int score{ 0 };
+	time_t ltime{ time(NULL) };
 };
 
-// error() simply disguises throws:
-inline void error(const string& s)
-{
-	throw runtime_error(s);
+//Global variables
+result lresult; //will be modified by each "operation"_op depending on right or wrong answer
+
+//Writing to results file/Record keeping
+ostream& operator<<(ostream& ost, result r) {
+	//overloading to write to file
+	//data will be stored as follows: ((score), (time)) where score is an integer and time is of the form (Www Mmm dd hh:mm:ss yyyy) followed by a '\n' and terminated with a null character
+
+	char buffer[32];			//since whenever using << on a result it will always be writing to file,
+								// will just take the time at the moment of writing to file with the operator
+	struct tm timeinfo;
+	localtime_s(&timeinfo, &r.ltime);
+	asctime_s(buffer, 32, &timeinfo);
+	ost << "score: " << r.score << '\n' << "date: " << buffer;
+	return ost;
+
 }
 
-inline void error(const string& s, const string& s2)
-{
-	error(s + s2);
+ostream& operator<<(ostream& ost, vector<op_range> r) {//adding settings to file, operations used and associated range for each
+
+	for (int i = 0; i < (int)r.size(); ++i) {
+		ost << op_to_string(r[i].op()) << ": " << "( " << r[i].lrange().low() <<  " , " << r[i].lrange().up() << " )"
+			<< ", " << "( " << r[i].rrange().low() << " , " << r[i].rrange().up() << " )" << '\n';
+	}
+	return ost;
 }
 
-inline void error(const string& s, int i)
-{
-	ostringstream os;
-	os << s << ": " << i;
-	error(os.str());
+istringstream& operator>> (istringstream& iss, vector<op_range>& r) {//read settings from file and use settings if requested
+
+	while (!iss.eof()) {
+		string temp;
+		iss >> temp;
+
+		int lrange_low{ 0 };
+		int lrange_up{ 0 };
+		int rrange_low{ 0 };
+		int rrange_up{ 0 };
+		char ch;
+
+		if (temp == "score:") {} //do nothing with score, keep going
+		else if (temp == "Date:") {}
+		else if (temp == "plus:") {//read the left and right ranges.
+			iss >> ch >> lrange_low >> ch >> lrange_up;
+			while (iss >> ch && ch != '('); //eat characters until you get to the proper position to read an int
+			iss >> rrange_low >> ch >> rrange_up >> ch;
+			r.push_back(op_range(operation::plus, range{ lrange_low, lrange_up }, range{ rrange_low, rrange_up }));
+		}
+		else if (temp == "minus:") {
+			iss >> ch >> lrange_low >> ch >> lrange_up;
+			while (iss >> ch && ch != '('); //eat characters until you get to the proper position to read an int
+			iss >> rrange_low >> ch >> rrange_up >> ch;
+			r.push_back(op_range(operation::minus, range{ lrange_low, lrange_up }, range{ rrange_low, rrange_up }));
+		}
+		else if (temp == "product:") {
+			iss >> ch >> lrange_low >> ch >> lrange_up;
+			while (iss >> ch && ch != '('); //eat characters until you get to the proper position to read an int
+			iss >> rrange_low >> ch >> rrange_up >> ch;
+			r.push_back(op_range(operation::product, range{ lrange_low, lrange_up }, range{ rrange_low, rrange_up }));
+		}
+		else if (temp == "division:") {
+			iss >> ch >> lrange_low >> ch >> lrange_up;
+			while (iss >> ch && ch != '('); //eat characters until you get to the proper position to read an int
+			iss >> rrange_low >> ch >> rrange_up >> ch;
+			r.push_back(op_range(operation::division, range{ lrange_low, lrange_up }, range{ rrange_low, rrange_up }));
+		}
+		else if (temp == "time_limit:") break; //will take care of time limit below
+
+	}
+	return iss;
 }
 
 
+//Numerical
+inline int get_rand(const range& range) {//provides a random number from the given range, called often.
+	return (rand() % (range.up() + 1 - range.low())) + range.low();
+}
+
+inline int flip_sign(int& x) {
+	range negative_chance = range(1, 5); // 1/5 chance of flipping a given number to its negative
+	if (get_rand(negative_chance) == 1) return x *= -1; // chance of flipping one of the numbers to negative. See definition of negative chance
+	else return x;
+}
+
+int find_latest(vector<string>& data) {//find where the last entry for "score:" is. This will let me read the last entry for settings instead of the first.
+	string temp = "";
+	istringstream iss;
+	for (int i = data.size() - 1; i >= 0; --i) {
+		iss.str(data[i]);
+		iss.clear();                 // clear fail and eof bits
+		iss.seekg(0, std::ios::beg);
+		iss >> temp; //only bother reading the first word, just looking for the position of the occurence of "score:" to know where the latest settings is in file
+		if (temp == "score:") return i;
+	}
+	return -1;
+}
+
+
+
+
+
+//User prompts and input
 int get_num() {//takes number given by user. Used for range, time limit, etc.
-	
-	int num=-1; //set to -1 for while loop check below in case cin>> num fails
+	int num;
 	cin >> num;
-	
-	while (!cin|| num<0) {//cin must be good() and num has a value that is reasonable ie. >0
+
+	while (!cin) {//cin must be good() and num has a value that is reasonable ie. >0
 							//keep asking until both are satisfied
-		cin >> num;
 		if (cin.bad()) error("Input to num is bad(). \n");
 		else if (cin.fail()) {
-			cout << "Improper input entered, try again. \n";
+			cout << "Non-numerical input entered, try again. \n";
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n'); //clear buffer
-			num = -1; //reset to failure case
+			num = -1; 
 		}
-		else if (num <= 0) cout << "Invalid number entered, must be greater than 0. Try again. \n";
+		else  error("Unknown error occurred at get_num().");
+		cin >> num;
 	}
-	
+
 	return num;
 }
-
 
 vector<range> get_range() {
 	//Function to get range for the numbers for operations.
@@ -134,21 +270,13 @@ vector<range> get_range() {
 
 	vector<range> user_range(2);
 	user_range[0] = lrange; user_range[1] = rrange;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n'); //clear buffer
 	return user_range;
 }
 
 
-inline int get_rand(const range& range) {//provides a random number from the given range, called often.
 
-	return (rand() % (range.up() + 1 - range.low())) + range.low();
-
-}
-
-
-
-//User prompts and input
 bool get_soln(const int& left,const int& right,const operation& op) {//takes the two operands and gets solution from user, returns true or false depending on correctness.
-
 	
 	int ans = get_num();
 	switch (op) {//check user ans against answer given the chosen operation and operands
@@ -206,15 +334,10 @@ bool get_yesno() {//prompts user for a yes or no
 	}
 }
 
-
-
-//User/default range
+//User get ops, associated range or default
 vector<op_range> get_ops() {
 
 	vector<op_range> user_op; //Will place chosen operations by user in this vector
-
-
-	
 
 	cout << "Addition? 'y' for yes, 'n' for no. \n";  
 	if (get_yesno()) {
@@ -240,30 +363,29 @@ vector<op_range> get_ops() {
 		else range_sub = get_range();
 		user_op.push_back(op_range(operation::minus, range_sub[0], range_sub[1]));
 	}
-	else {
-
-	}
+	else {}
 
 	cout << "Multiplication? 'y' for yes, 'n' for no. \n";   
 	if (get_yesno()) {
-		vector<range> range_multi{range(2,12), range(2,100)};
+		
+		vector<range> range_multi{ range (2, 12), range (2, 100) };
 		cout << "Default range for multiplication? Left: (2,12) Right: (2,100). \n";
 		if (get_yesno()) {
 
 		}
 		else range_multi = get_range();
+
 		user_op.push_back(op_range(operation::product, range_multi[0], range_multi[1]));
-	}
-	else {
 
 	}
+	else {}
 
 	cout << "Division? 'y' for yes, 'n' for no. \n";   
 	if (get_yesno()) {
 		vector<range> range_div{ range(2,100), range(2,12) };
 		cout << "Default range for division? Left: (2,100) Right: (2,12). \n";
 		if (get_yesno()) {
-
+			
 		}
 		else range_div = get_range();
 
@@ -277,15 +399,67 @@ vector<op_range> get_ops() {
 
 		user_op.push_back(op_range(operation::division, range_div[0], range_div[1]));
 	}
-	else {
-
-	}
-
+	else {}
 	return user_op;
 }
-//User/default range
 
+vector<op_range> get_settings(string& iofile, int& time_limit) {
+	//search for results file
+	cout << "Do you have a previous results file? y or n \n";
+	vector<op_range> opernums;
 
+	char ans = 'n'; //will be used to check if the answer to above is yes no for later
+	if (get_yesno()) {
+		ans = 'y';
+		cout << "File name? \n";
+		getline(cin, iofile);
+
+		ifstream ist{ iofile };//do a quick check that the given files can be opened and written to then close. This prevents doing the test, having a score but being unable to write it in the end.
+		if (!ist) error("Cannot open file for reading! \n");
+		vector<string> data;
+		string temp;
+
+		while (!ist.eof()) {//partitioning the results file into each nonempty line from the beginning.
+			getline(ist, temp);
+			data.push_back(temp);
+		}
+		temp = "";
+
+		for (int i = find_latest(data); i < data.size(); ++i) {
+			temp += data[i] + " ";
+		}
+
+		istringstream iss{ temp };
+		iss >> opernums;
+		iss.str(data[data.size() - 3]);//jump to the line that is of the form "time_limit: xx" where xx is some integer. Due to formatting this is the third last line.
+		iss >> temp >> time_limit;
+
+		cout << "The following settings were found: \n";
+		for (op_range x : opernums) {
+			cout << op_to_string(x.op()) << " Left range: " << x.lrange().low() << '-' << x.lrange().up() << " Right range: " << x.rrange().low() << '-' << x.rrange().up() << '\n';
+		}
+		cout << "Time limit: " << time_limit << "\n"
+			<< "Do you want to proceed with these settings? \n";
+		if (get_yesno()) {}
+		else {
+			opernums = get_ops();
+			cout << "Time limit? (In seconds) \n";
+			time_limit = get_num();
+		}
+
+		ofstream ost{ iofile, ios_base::app };
+		if (!ost) error("Cannot open file for writing! \n");
+	}
+
+	else {
+		opernums = get_ops();
+		cout << "Time limit? (In seconds) \n";
+		time_limit = get_num();
+	}
+
+	return opernums;
+}
+//User prompts and input
 
 
 //Operations
@@ -293,6 +467,9 @@ void plus_op(const range& lrange, const range& rrange) {
 
 	int left_num = get_rand(lrange); //generating a number in the given range
 	int right_num = get_rand(rrange);
+
+	if (get_rand(range(1, 2)) == 1) left_num = flip_sign(left_num);//flip a coin to decide which number to have a chance at being negative
+	else right_num = flip_sign(right_num);
 
 	while (true) {//keep asking for input until gets question right, check_time prevents going past time limit despite incorrect attempts
 		cout << left_num << '+' << right_num << "? \n";
@@ -304,7 +481,7 @@ void plus_op(const range& lrange, const range& rrange) {
 
 		else {
 			cout << "Incorrect (-2). Try again. \n";
-			lresult.score--;
+			lresult.score-=2;
 		}
 	}
 		
@@ -312,7 +489,10 @@ void plus_op(const range& lrange, const range& rrange) {
 
 void minus_op(const range& lrange, const range& rrange) {
 	int left_num = get_rand(lrange); //generating a number in the given range
-	int right_num = get_rand(range(rrange.low(), left_num)); //send a new range such that right_num < left_num 
+	int right_num = get_rand(rrange); //Note resulting difference may be negative, this is a deliberate choice.
+
+	if (get_rand(range(1, 2)) == 1) left_num = flip_sign(left_num);//flip a coin to decide which number to have a chance at being negative
+	else right_num = flip_sign(right_num);
 
 	while (true) {
 		cout << left_num << '-' << right_num << "? \n";
@@ -324,7 +504,7 @@ void minus_op(const range& lrange, const range& rrange) {
 
 		else {
 			cout << "Incorrect (-2). Try again. \n";
-			lresult.score--;
+			lresult.score-=2;
 		}
 	}
 }
@@ -333,6 +513,7 @@ void minus_op(const range& lrange, const range& rrange) {
 void multi_op(const range& lrange, const range& rrange) {
 	int left_num = get_rand(lrange); //no need for restriction on left and right num
 	int right_num = get_rand(rrange);
+
 
 	while (true) {
 		cout << left_num << '*' << right_num << "? \n";
@@ -344,12 +525,13 @@ void multi_op(const range& lrange, const range& rrange) {
 
 		else {
 			cout << "Incorrect (-2). Try again. \n";
-			lresult.score--;
+			lresult.score-=2;
 		}
 	}
 }
 
 void div_op(const range& lrange, const range& rrange) {
+
 	int right_num = get_rand(rrange); //need left num to be a multiple of right num, handled here
 	int quotient = lrange.up() / right_num; //check how many times right_num goes into the upper bound of left number
 
@@ -366,98 +548,63 @@ void div_op(const range& lrange, const range& rrange) {
 
 		else {
 			cout << "Incorrect (-2). Try again. \n";
-			lresult.score--;
+			lresult.score-=2;
 		}
 	}
 }
 //Operations
 
 
-
-ostream& operator<<(ostream& ost, result r) {
-	//overloading to write to file
-	//data will be stored as follows: ((score), (time)) where score is an integer and time is of the form (Www Mmm dd hh:mm:ss yyyy) followed by a '\n' and terminated with a null character
-
-	char buffer[32];			//since whenever using << on a result it will always be writing to file,
-								// will just take the time at the moment of writing to file with the operator
-	struct tm timeinfo;
-	localtime_s(&timeinfo, &r.ltime);
-	asctime_s(buffer, 32, &timeinfo);
-	buffer[24] == ')'; //asctime's format makes the characters in pos 24 a  '\n'. This breaks formatting if one wants to put a bracket afterwards. Hence changing it to a bracket
-	ost << '(' << r.score << ',' << buffer;
-	return ost;
-
-}
-
 int main() {
 	try{
-
-		//search for results file
-		cout << "Do you have a previous results file? \n";
 		string iofile;
-		char ans = 'n'; //will be used to check if the answer to above is yes no for later
-		if (get_yesno()) {
-			ans = 'y';
-			cout << "File name? \n";
-			getline(cin, iofile);
+		int time_limit;
 
-			ifstream ist{ iofile };//do a quick check that the given files can be opened and written to then close. This prevents having a score but being unable to write it in the end.
-			if (!ist) error("Cannot open file for reading! \n");
-
-			ofstream ost{ iofile };
-			if (!ost) error("Cannot open file for writing! \n");
-		}
-	
-		vector<op_range> opernums = get_ops();
+		vector<op_range> opernums = get_settings(iofile, time_limit);
 		if (opernums.size() == 0) {
 			cout << "No opertions entered. Closing program. \n";
 			return 0;
 		}
-
-		cout << "Time limit? (In seconds) \n";
-		int time_limit = get_num();
-
-		
-
-
+	
 		time_t start_time{ time(NULL) };//Will be checked against multiple times, ex. after question is answered
 		srand((int)start_time); //Generating random seed for rand() based on current time
 
 		//main loop to randomly pick what operations to give
 		time_t current_time;
+		operation op;
+		int position;
 		while (difftime(time(&current_time),start_time)<time_limit) {
-			operation op = opernums[(rand() % opernums.size())].op();  // Number to choose which ops ex. if all 4 operations then number between 1-4 then pick that position in the vector of opranges
+			position = (rand() % opernums.size()); // Number to choose which ops ex. if all 4 operations then number between 1-4 then pick that position in the vector of opranges
+			op = opernums[position].op(); 
 
 			switch (op){
 			case operation::plus:
-				plus_op(opernums[0].lrange(), opernums[0].rrange());
+				plus_op(opernums[position].lrange(), opernums[position].rrange());
 				break;
 
 			case operation::minus:
-				minus_op(opernums[0].lrange(), opernums[0].rrange());
+				minus_op(opernums[position].lrange(), opernums[position].rrange());
 				break;
 
 			case operation::product:
-				multi_op(opernums[0].lrange(), opernums[0].rrange());
+				multi_op(opernums[position].lrange(), opernums[position].rrange());
 				break;
 
 			case operation::division:
-				div_op(opernums[0].lrange(), opernums[0].rrange());
+				div_op(opernums[position].lrange(), opernums[position].rrange());
 				break;
 			}
 		}
 		
 		cout << "Score is: " << lresult.score;
 		lresult.ltime = time(NULL);
-		if (ans == 'y') {
-			ofstream ost{ iofile };
+		if (iofile != "") {//not the empty string means we must've written down someplace to write results to when get_settings was called
+			ofstream ost{ iofile, ios_base::app };
 			if (!ost) error("Cannot open file for writing! File Tampered after entry! \n");
-			ost << lresult; //see << operator overload above for the format of record keeping
+			ost << lresult << opernums << "time_limit: " << time_limit << "\n\n"; //see << operator overload above for the format of record keeping
 		}
 	}
 	
-	
-
 	catch (runtime_error& e) {
 		cerr << "Runtime error: " << e.what() << "\n";
 		return 1;
@@ -466,8 +613,5 @@ int main() {
 		cerr << "Exception: something went wrong. \n";
 		return 2;
 	}
-
-	
-
 }
 
